69240f3ae66b14d68ac17d3d7bdb3d1a66af605f
Anders Ringqvist <anders.ringqvist@gmail.com>
2023-04-07 22:50:03 +0200

    enable meow

--- dist2/index.js
+++ dist2/index.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
 import React from "react";
 import { render } from "ink";
-//import meow from "meow";
+import meow from "meow";
 import terminalImage from "terminal-image";
 import App from "./app.js";
 import { __dirname } from "./utils.js";
-
-/*
-meow(
-	`
+meow(`
 	Usage
 	  $ anddoutoi
-`,
-	{importMeta: import.meta},
-);
-*/
+`, {
+  importMeta: import.meta
+});
 console.log(await terminalImage.file(`${__dirname(import.meta.url)}/../assets/avatar.jpg`, {
   width: 48
 }));

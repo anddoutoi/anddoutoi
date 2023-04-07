36e5f2887c424a8b83010947d275eeb7acaf1156
Anders Ringqvist <anders.ringqvist@gmail.com>
2023-04-07 21:21:15 +0200

    Use dist again

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-
 import React from "react";
 import {render} from "ink";
 //import meow from "meow";
 import terminalImage from "terminal-image";
 import App from "./app.js";
+import {__dirname} from "./utils.js";
 
 /*
 meow(
@@ -15,7 +15,6 @@ meow(
 	{importMeta: import.meta},
 );
 */
-
-console.log(await terminalImage.file("./assets/avatar.jpg", {width: 48}));
+console.log(await terminalImage.file(`${__dirname(import.meta.url)}/../assets/avatar.jpg`, {width: 48}));
 
 render(<App />);
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+import {dirname} from "node:path";
+import {fileURLToPath} from "node:url";
+
+function __dirname(url) {
+	return dirname(__filename(url));
+}
+
+function __filename(url) {
+	return fileURLToPath(url);
+}
+
+export {
+	__dirname,
+	__filename,
+};
